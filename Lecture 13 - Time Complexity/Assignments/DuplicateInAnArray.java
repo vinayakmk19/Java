eb53b8a6bc89b74aee585ba28910f57eb4f208c0
vinayakmk19 <110101863+vinayakmk19@users.noreply.github.com>
2023-06-03 19:04:57 +0530

    Add files via upload

---
+++ Lecture 13 - Time Complexity/Assignments/DuplicateInAnArray.java
@@ -0,0 +1,136 @@
+/*
+ 								***** Duplicate in array *****
+
+	You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3, and among these, there is a single integer value that is present twice. You need to find and return that duplicate number present in the array.
+	Note :
+	Duplicate number is always present in the given array/list.
+	Input format :
+	The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+	
+	First line of each test case or query contains an integer 'N' representing the size of the array/list.
+	
+	Second line contains 'N' single space separated integers representing the elements in the array/list.
+	Output Format :
+	For each test case, print the duplicate element in the array/list.
+	
+	Output for every test case will be printed in a separate line.
+	Constraints :
+	1 <= t <= 10^2
+	0 <= N <= 10^6
+	
+	Time Limit: 1 sec
+	Sample Input 1:
+	1
+	9
+	0 7 2 5 4 7 1 3 6
+	Sample Output 1:
+	7
+	Sample Input 2:
+	2
+	5
+	0 2 1 3 1
+	7
+	0 3 1 5 4 3 2
+	Sample Output 2:
+	1
+	3
+
+
+ */
+
+package assignments;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution2 {
+
+	/*
+	 * Approach 1:
+	 */
+
+//	public static int findDuplicate(int[] arr) {
+//
+//		int sum = 0;
+//		for (int i = 0; i < arr.length; i++) {
+//			sum = sum + arr[i];
+//		}
+//		int n = arr.length;
+//		int sumOfNaturalNumbers = ((n - 1) * (n - 2)) / 2;
+//		return sum - sumOfNaturalNumbers;
+//	}
+
+	/*
+	 * Approach 2:
+	 */
+
+//	public static int findDuplicate(int[] arr) {
+//
+//
+//		for (int i = 0; i < arr.length; i++) {
+//			for (int j = 0; j < arr.length; j++) {
+//				if (i !=j && arr[i] == arr[j]) {
+//					return arr[i];
+//				}
+//			}
+//		}
+//		return -1;
+//	}
+
+	/*
+	 * Approach 3:
+	 */
+
+	public static int findDuplicate(int[] arr) {
+		int n = arr.length - 2;
+		int sum = 0;
+		for (int i : arr) {
+			sum += i;
+		}
+		return sum - (n * (n + 1) / 2);
+	}
+}
+
+public class DuplicateInAnArray {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static int[] takeInput() throws IOException {
+		int size = Integer.parseInt(br.readLine().trim());
+		int[] input = new int[size];
+
+		if (size == 0) {
+			return input;
+		}
+
+		String[] strNums;
+		strNums = br.readLine().split("\\s");
+
+		for (int i = 0; i < size; ++i) {
+			input[i] = Integer.parseInt(strNums[i]);
+		}
+
+		return input;
+	}
+
+	public static void printArray(int[] arr) {
+		for (int element : arr) {
+			System.out.print(element + " ");
+		}
+
+		System.out.println();
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int t = Integer.parseInt(br.readLine().trim());
+
+		while (t > 0) {
+
+			int[] input = takeInput();
+			System.out.println(Solution2.findDuplicate(input));
+
+			t -= 1;
+		}
+	}
+}
